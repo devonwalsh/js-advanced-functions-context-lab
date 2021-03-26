42000971cfac7abe3067c2b8af33e643612dd425
Devon Walsh <devonwalsh@gmail.com>
2021-03-25 23:14:42 -0400

    commit tree bb6a0ad6573b773b6c1565bd4a2cd71c70cdbc40

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
  for you to use if you need it!
  */
 
-let allWagesFor = function () {
+ let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -19,4 +19,75 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(array) {
+    let obj = [];
+    
+    array.forEach(employee => {
+        let newEmployee = createEmployeeRecord(employee);
+        obj.push(newEmployee);
+    });
+    
+    return obj;
+}
+
+function createTimeInEvent(timeStamp) {
+
+    let dateTime = timeStamp.split(' ');
+    let timeIn = {
+        type: "TimeIn",
+        hour: parseInt(dateTime[1]),
+        date: dateTime[0]
+    }
+
+    this['timeInEvents'].push(timeIn);
+    return this;
+}
+
+function createTimeOutEvent(timeStamp) {
+
+    let dateTime = timeStamp.split(' ');
+    let timeOut = {
+        type: "TimeOut",
+        hour: parseInt(dateTime[1]),
+        date: dateTime[0]
+    }
+
+    this['timeOutEvents'].push(timeOut);
+    return this;
+}
+
+function hoursWorkedOnDate(recordDate) {
+    let clockIn = this["timeInEvents"].find(obj => obj.date === recordDate)
+    let clockOut = this["timeOutEvents"].find(obj => obj.date === recordDate)
+    return (clockOut.hour - clockIn.hour)/100;
+}
+
+function wagesEarnedOnDate(recordDate) {
+    let hoursWorked = hoursWorkedOnDate.call(this, recordDate)
+    return hoursWorked * this["payPerHour"];
+}
+
+function findEmployeeByFirstName(srcArray, inputName) {
+    return srcArray.find(obj => obj.firstName === inputName)
+}
+
+function calculatePayroll(employeeRecords) {
+    let employeeWages = []
+    employeeRecords.forEach(record => employeeWages.push(allWagesFor.call(record)));
+
+    let sum = (accumulator, currentValue) => accumulator + currentValue;
+    return employeeWages.reduce(sum);
 }
